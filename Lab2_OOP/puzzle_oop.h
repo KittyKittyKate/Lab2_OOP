4328cd2b18794446564a2f128f7bc7b7d0316a08
KittyKittyKate <ivasam248@gmail.com>
2021-02-24 20:04:26 +0700

    Новый класс

---
+++ Lab2_OOP/puzzle_oop.h
@@ -0,0 +1,18 @@
+#ifndef __puzzle__
+#define __puzzle__
+
+#include "wisdom_oop.h"
+namespace collection_of_wisdom_oop {
+	
+	class puzzle : public wisdom {
+		char puzzle_str[200];
+		char answer[20];
+	public:
+		// переопределяем интерфейс класса
+		void InData(ifstream &ifst); // ввод
+		void Out(ofstream &ofst); // вывод
+		puzzle() {} // создание без инициализации.
+	};
+} // end collection_of_wisdom_oop namespace
+#endif
+
