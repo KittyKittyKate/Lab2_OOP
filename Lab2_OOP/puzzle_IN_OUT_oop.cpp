9b4457be3a75d8946455421f13c32dc86382f374
KittyKittyKate <ivasam248@gmail.com>
2021-03-04 15:25:33 +0700

    В in.txt была добавлена загадка. Изменен out.txt соответственно. В класс puzzle добавлена функция Count_Comma().

--- Lab2_OOP/puzzle_IN_OUT_oop.cpp
+++ Lab2_OOP/puzzle_IN_OUT_oop.cpp
@@ -11,4 +11,16 @@ namespace collection_of_wisdom_oop {
 	void puzzle::Out(ofstream &ofst) {
 		ofst << "'" << this->puzzle_str << "' - Answer: " << this->answer << endl;
 	}
+	int puzzle::Count_Comma() {
+		int comma = 0;
+		int i = 0;
+		while (this->puzzle_str[i] != '\0')
+		{
+			if (this->puzzle_str[i] == ',') {
+				comma++;
+			}
+			i++;
+		}
+		return comma;
+	}
 } // end collection_of_wisdom_oop namespace
